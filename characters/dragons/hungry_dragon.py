c724addf2bff73f5a4b8d014a972bc751053f4d2
praveen kumar <ppraveen@iitk.ac.in>
2021-07-09 19:13:07 +0530

    2.3
    Implemented hungry_dragon

--- characters/dragons/hungry_dragon.py
+++ characters/dragons/hungry_dragon.py
@@ -1,27 +1,40 @@
 from .dragon import Dragon
-
+from utils import random_or_none
 
 class HungryDragon(Dragon):
     """HungryDragon will take three turns to digest a Terminator in its place.
     While digesting, the HungryDragon can't eat another Terminator.
     """
     name = 'Hungry'
+    food_cost = 4
+    time_to_digest = 3
     # OVERRIDE CLASS ATTRIBUTES HERE
     # BEGIN 2.3
-    implemented = False  # Change to True to view in the GUI
+    implemented = True  # Change to True to view in the GUI
 
     # END 2.3
 
     def __init__(self, armor=1):
         # BEGIN 2.3
+        self.digesting = 0
+        self.armor = armor
         "*** YOUR CODE HERE ***"
         # END 2.3
 
     def eat_terminator(self, terminator):
         # BEGIN 2.3
+        if terminator!= None:
+            terminator.reduce_armor(terminator.armor)
+            self.digesting = self.time_to_digest
         "*** YOUR CODE HERE ***"
         # END 2.3
 
     def action(self, colony):
         # BEGIN 2.3
+        s=self.place
+        t=(s.terminators)[:]
+        if self.time_to_digest>0 and self.digesting >0:
+            self.digesting-=1
+        else:
+            self.eat_terminator(random_or_none(t))
         "*** YOUR CODE HERE ***"
